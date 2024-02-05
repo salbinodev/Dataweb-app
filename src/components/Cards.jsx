b899164340f8a25eca8af1e6073e325ea31a994d
salbinodev <salbinoo@outlook.com>
2024-02-05 22:15:44 +0200

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className='w-full py-[10rem] px-4 bg-white'>
+        <div className='max-w-[1240px] mx-auto'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Cards
